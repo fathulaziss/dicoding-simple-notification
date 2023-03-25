07d57e7b7241b0eb64d5cd524a09c0504b8297dc
Muhamad Fathul Azis <azisfathl@gmail.com>
2023-03-25 13:03:56 +0700

    update

--- lib/ui/detail_page.dart
+++ lib/ui/detail_page.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: cast_nullable_to_non_nullable
-
 import 'package:dicoding_simple_notification/data/models/received_notification_model.dart';
 import 'package:flutter/material.dart';
 
@@ -10,8 +8,10 @@ class DetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final args =
-        ModalRoute.of(context)?.settings.arguments as ReceivedNotificationModel;
+    final args = ModalRoute.of(context)?.settings.arguments != null
+        ? ModalRoute.of(context)!.settings.arguments!
+            as ReceivedNotificationModel
+        : ReceivedNotificationModel();
 
     return Scaffold(
       appBar: AppBar(title: Text('Title: ${args.payload}')),
